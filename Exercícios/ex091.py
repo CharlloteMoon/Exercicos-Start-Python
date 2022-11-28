5e3ff767cffa5d42de13e5fdc8d1eb2a14b9927e
Charllote Moon <charllotemoon@gmail.com>
2022-11-28 16:57:17 -0300

    Update ex091.py

--- Exercícios/ex091.py
+++ Exercícios/ex091.py
@@ -1,15 +1,34 @@
 print()
 
 from random import randint
+from time import sleep
 
-print(f'{" Exercicio 91 ":-^30} \n{" Jogo de Dados em Python ":^30} \n')
+print(f'{" Exercicio 91 ":-^40} \n{" Jogo de Dados em Python ":^40} \n')
 
-infoJogadores = []
+infoJogadores = {}
 
 for jogos in range(1, 5):
 
-    infoJogadores['jogo'] = randint(1, 6)
+    infoJogadores[f'Jogador {jogos}'] = randint(1, 6)
 
-print(infoJogadores)
+for k, v in infoJogadores.items():
+
+    sleep(0.5)
+
+    print(f'{f" O {k}  tirou o número {v} ":^40}')
+
+sleep(1)
+
+print(f'\n{" RANKING ":-^40} \n')
+
+count = 1
+
+for i in sorted(infoJogadores, key = infoJogadores.get, reverse=True):
+    
+    sleep(0.5)
+
+    print(f'{f" {count}º Lugar - {i.capitalize()} com o {infoJogadores[i]} ":^40}')
+
+    count += 1
 
 print()
